6ae81604663e75a7de125c15aea1fde4e70b9ea6
Shota <sh.klibadze@gmail.com>
2023-03-06 19:24:04 +0400

    Initial Commit

---
+++ FootballPlayers/FootballPlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballPlayersApp.swift
+//  FootballPlayers
+//
+//  Created by Shotiko Klibadze on 06.03.23.
+//
+
+import SwiftUI
+
+@main
+struct FootballPlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
